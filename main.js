c0efa6114b39b22426143442ed6fe1ba3d1f92ff
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-10-20 12:29:23 +0700

    fix: remove page loader component, relocate Json+ld script to head, replace data.json with data.js

--- main.js
+++ main.js
@@ -1,31 +1,15 @@
-// Loading Screen
-const loadingScreen = document.getElementById("loading-screen");
-const mainContent = document.getElementById("main-content");
-
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    loadingScreen.style.display = "none";
-    mainContent.style.display = "block";
-  }, 2000);
-});
-
-// Display Current Year
-const yearText = document.getElementById("year");
-const date = new Date();
-let year = date.getFullYear();
-yearText.innerText = year;
+import { projects, testimonials } from "./data.js";
 
 // Display Projects and Testimonial Cards
 const projectsWrapper = document.getElementById("projects-wrapper");
 const testimonialsWrapper = document.getElementById("testimonials-wrapper");
 
-function displayItems(data) {
-  // Projects
-  data.projects.map((project) => {
-    const projectCard = document.createElement("div");
-    projectCard.classList.add("project-card");
+// Projects
+projects.map((project) => {
+  const projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
 
-    projectCard.innerHTML = `
+  projectCard.innerHTML = `
     <div class="project-card-header">
         <img
           src="${project.img}"
@@ -46,15 +30,15 @@ function displayItems(data) {
         </div>
     </div>`;
 
-    projectsWrapper.appendChild(projectCard);
-  });
+  projectsWrapper.appendChild(projectCard);
+});
 
-  // Testimonials
-  data.testimonials.map((testimonial) => {
-    const testimonialCard = document.createElement("div");
-    testimonialCard.classList.add("testimonial-card");
+// Testimonials
+testimonials.map((testimonial) => {
+  const testimonialCard = document.createElement("div");
+  testimonialCard.classList.add("testimonial-card");
 
-    testimonialCard.innerHTML = `    
+  testimonialCard.innerHTML = `    
     <div class="testimonial-card-header">
       <img
         src="${testimonial.img}"
@@ -73,13 +57,8 @@ function displayItems(data) {
     </p>
 `;
 
-    testimonialsWrapper.appendChild(testimonialCard);
-  });
-}
-
-fetch("data.json")
-  .then((response) => response.json())
-  .then((data) => displayItems(data));
+  testimonialsWrapper.appendChild(testimonialCard);
+});
 
 // Toast
 function showToast(message, type) {
@@ -204,3 +183,9 @@ form.addEventListener("submit", (event) => {
       submitButton.disabled = false; // Re-enable the submit button
     });
 });
+
+// Display Current Year
+const yearText = document.getElementById("year");
+const date = new Date();
+let year = date.getFullYear();
+yearText.innerText = year;
